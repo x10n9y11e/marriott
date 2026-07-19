ed2547078d77a08d6a02e0cc34bbbe29af09a3d9
TX <60465554+x10n9y11e@users.noreply.github.com>
2026-07-19 20:10:33 +0800

    Update EKS module variable names for consistency

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,14 +26,14 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
+  name    = var.cluster_name
+  kubernetes_version = var.cluster_version
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
+  endpoint_public_access  = true
+  endpoint_private_access = true
 
   eks_managed_node_groups = {
     default = {
