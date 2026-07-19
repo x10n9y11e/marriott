67100d045449e6f193219dd6a5101b3b6dd2ed82
TX <60465554+x10n9y11e@users.noreply.github.com>
2026-07-19 18:38:02 +0800

    Update VPC and EKS module versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 5.13"
 
   name = "${var.cluster_name}-vpc"
   cidr = var.vpc_cidr
@@ -24,7 +24,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+  version = "~> 20.39"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
